6bc7bd80dc8402e29c541d1a737fd6ccecbb27d0
Shiluba02 <shiluba0202ndhlovu@gmail.com>
2023-07-13 16:15:36 -0700

    5print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - AA program that prints a line using the printf function
+ * main - A program that print a line using printf function
  * Return: 0 (Success)
  */
 
